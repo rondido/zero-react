fc9d96c23fb0d1bbc0d74f601e9c39785c8f1c50
rondido <rppr001@naver.com>
2022-09-11 15:28:27 +0900

    useEffect and lifecycle commit

--- CRA/my-app/src/App.js
+++ CRA/my-app/src/App.js
@@ -1,13 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
-import Calculator from "./components/Calculator";
-
+//import Calculator from "./components/Calculator";
+//import Clock from "./components/Clock";
 //import Counter from "./components/Counter";
 //import Counter2 from "./components/Counter2";
-
+import FunctionClock from "./components/FunctionClock";
 function App() {
+  useEffect(() => {}, []);
   return (
     <div className="App">
-      <Calculator />
+      <FunctionClock />
     </div>
   );
 }
