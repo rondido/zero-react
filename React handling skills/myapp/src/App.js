593707bc34cfd1cc93e5c2044ed951a06b66e298
rondido <rppr001@naver.com>
2022-09-22 23:52:12 +0900

    React ref commit

--- React handling skills/myapp/src/App.js
+++ React handling skills/myapp/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Say from "./Say";
+import EventPractice from "./EventPractice";
+import EventPractice_1 from "./EventPractice_1";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Say />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
