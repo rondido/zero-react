e512c216c8b214587f44255351e95607402f9974
rondido <rppr001@naver.com>
2022-09-23 11:48:08 +0900

    React component loop commit

--- React handling skills/myapp/src/App.js
+++ React handling skills/myapp/src/App.js
@@ -5,13 +5,15 @@ import EventPractice_1 from "./EventPractice_1";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 import { Component } from "react";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollBottom()}>맨 밑으로</button>
+        {/* <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollBottom()}>맨 밑으로</button> */}
+        <IterationSample />
       </div>
     );
   }
